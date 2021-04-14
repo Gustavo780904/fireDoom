443fdefbcc92534e2eb5b7fe689fd9a2a85d260d
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-04-13 23:42:55 -0300

    renderfire

--- js/fire.js
+++ js/fire.js
@@ -2,8 +2,8 @@ const firePixelsArray = []
 const fireWidth = 2
 const fireHeight = 3
 function start() {
-createFireDataStructure()
-console.log(firePixelsArray)
+    createFireDataStructure()
+renderFire()
 }
 function createFireDataStructure() {
     const numberOfPixels = fireWidth * fireHeight
@@ -14,7 +14,20 @@ function createFireDataStructure() {
 function calculateFirePropagation() {
 
 }
-function renderFire(){
-
+function renderFire() {
+    let html = '<table cellpadding="0" cellspacing="0">'
+    for (let row =0; row < fireHeight; row++) {
+        html += '<tr>'
+// isere a quebra de linha
+        for (let column = 0; column < fireWidth; column++) {
+           const pixelIndex = column + (fireWidth*row)
+            html += '<td>'
+           html +=pixelIndex
+            html += '</td>'
+        }
+        html += '</tr>'
+    }
+    html += '</table>'
+    document.querySelector('#fireCanvas').innerHTML= innerHTML
 }
 start()
